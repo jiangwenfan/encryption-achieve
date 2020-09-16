8120c0ab2e31ea0149433bc7deee263f1c7a482a
Your Name <you@example.com>
2020-09-16 15:48:11 +0800

    基于字符串加密的初步实现

--- C encryption achieve/CaesarCipher.c
+++ C encryption achieve/CaesarCipher.c
@@ -1,66 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 
+char cipher1[100];
 void InputPlaintext();
-void InputKey();
+int InputKey();
 void Input();
-void OutputCiphertext();
+void OutputCiphertext(ciphertext,i);
+int JudgmentCharacter(int AsciiCode);
+char AlgorithmAchieve(plaintext, key);
 int main()
 {
 	printf("这是凯撒加密的C语言实现！\n");
 	Input();
-	OutputCiphertext();
+	//OutputCiphertext();
 	return 0;
 }
 //明文接收函数
 void InputPlaintext()
 {
 	printf("请输入要加密的明文\n");
-	char plaintext[100]; //明文接收数组
+	char plaintext[1000]; //明文接收数组
+	//gets()
 	gets(plaintext); //接收明文
+	int key = InputKey(); //接受密钥key
 	int len = strlen(plaintext); //判断明文数组长度
 	//迭代字符串数组中的每一个字符
 	for (int i = 0; i < len; i++)
 	{
-		int res = juage(plaintext[i]);
-		printf("%d\n",res);
-		printf("--:%c\n",plaintext[i]);
+		int AsciiCode = plaintext[i];
+		//int res = JudgmentCharacter(AsciiCode);
+		char cipher = AlgorithmAchieve(plaintext[i], key);
+		OutputCiphertext(cipher,i);
+		
+
 	}
-	printf("length:%d\n", len);
-	printf("show:%s\n",plaintext);
-	//判断字符是否非法;
-	int juage(char a){
-		int aa = a; //assii码
-		if (aa < 32 && a < 126)
-		{
-			return 1;
-			//合法字符
-		}
-		else
-		{
-			return 0;
-		};
-	}
+	printf("\n密文是:%s\n", cipher1);
+	//printf("[test]length:%d\n", len);
+	//printf("[test]show:%s\n",plaintext);
+	
+	
 	
 }
 //密钥key接收函数
-void InputKey()
+int InputKey()
 {
 	printf("请输入要加密的密钥\n");
+	int Key=0;
+	scanf("%d",&Key);
+	return Key;
 }
 //输入函数
 void Input()
 {
 	InputPlaintext();
-	InputKey();
+	//InputKey();
 }
 //输出密文函数
-void OutputCiphertext()
+void OutputCiphertext(ciphertext,i)
 {
-	printf("输出密文\n");
+	//printf("输出密文\n");
+	cipher1[i] = ciphertext;
+	
+}
+//判断字符是否非法;
+int JudgmentCharacter(int AsciiCode)
+{
+	//printf("[test]:%d\n",AsciiCode);
+	//if ((int)AsciiCode < 32 && (int)AsciiCode < 126)
+	//{
+	//	printf("keyi....");
+	//	return 1;
+	//	//合法字符
+	//} 
+	//else
+	//{
+	//	return 0;
+	//}
 }
 //算法的具体实现
-void AlgorithmAchieve(plaintext,key)
+char AlgorithmAchieve(plaintext,key)
 {
-
+	//printf("明文:%c\n",plaintext);
+	char ciphertext = plaintext + key;
+	//printf("密文:%c\n", ciphertext);
+	return ciphertext;
 }
--- C encryption achieve/test.c
+++ C encryption achieve/test.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
-#include <string.h>
+//#include <string.h>
 void main1() {
 	/*char a = 'a';
 	printf("%d",a);*/
 	//char name[3] = {'1','2','3'};
 	//int age[3] = { 1,2,3 };
-	char na[100];
-	char name2[] = "abc";
-	//char name[] = { 'a','a','a' };
-	int len = strlen(name2);
-	printf("----%d----",len);
-	printf("%c",name2[1]);
+	//char na[100];
+	//char name2[] = "abc";
+	////char name[] = { 'a','a','a' };
+	//int len = strlen(name2);
+	//printf("----%d----",len);
+	//printf("%c",name2[1]);
+	//char name[10];
+	//gets(name);
 	/*for (int i = 0; i < strlen(name); i++)
 	{
-		printf("%d\n",name[i]);
+		int code = name[i];
+		printf("%d\n",code);
 	}*/
+	//char ch[10];
+	//gets(ch);
+	/*char ch[10];
+	gets(ch);
+	for(int i)*/
+	//int a = 3;
+	//int b = 10;
+
+	//int c = 4;
+	//if (c<b || c<a)
+	//{
+	//	printf("c");
+	//}
 }
 
 /*
